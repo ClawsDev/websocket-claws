d1331eefd215cad9749aa0967832d2910956cf61
ClawsDev <gfxbros72@gmail.com>
2018-06-01 22:34:08 -0300

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const socket = require('socket.io')
+
+//app setup
+const app = express();
+const server = app.listen(5000, function(){
+    console.log('Rodando na porta 5000...')
+});
+
+
+
+
+//static files
+
+app.use(express.static('public'))
+
+//socket setup
+const io = socket(server)
+
+io.on('connection', function(socket){
+    console.log('Conexao socket realizada...',socket.id)
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data)
+    });
+});
